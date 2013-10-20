60fbaf1596f2f153f9aea835bb5d4a1a5412fe96
Michael Swiger <mokkan@wasome.com>
2013-10-19 17:57:01 -0700

    Restructure code a bit

--- main.c
+++ main.c
@@ -1,81 +1,71 @@
 /*
- * Project 3
- * Scheduler
- * CS460
- * Corey Amoruso
- * Michael Swiger
- * Sasha Demiyanik*/
+ * CS460 Project 3
+ * Scheduler Sim
+ * Corey Amoruso, Michael Swiger, Sasha Demiyanik
+ */
 
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#define DEBUG 1
-
-int nproc = 0;
-int cs = 0;
-char* type = NULL;
-int q = 0;
-
-void checkArguments(int argc,char *argv[]){
-
-	//checks to see the minimum number of arguemnts was supplied
-	if(argc < 3){
-		printf("Not enough arguments supplied. Please use format: ./SchedSim (int)nproc (int)cs (str)type [(int) q)]\n");
-		exit(1);
-	}
-
-	//checks the nproc variable for correctness
-	nproc = atoi(argv[1]);
-
-	if(DEBUG){ printf("nproc = %d\n", nproc);}
-
-	while(nproc < 0 ){
-		printf("Error: You specified less than zero process are to be simulated. Please specify nproc > 0.\nnproc = ");
-		fscanf(stdin, "%d", &nproc);
-		if(DEBUG){ printf("nproc = %d\n", nproc);}
-	}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-
-	//checks the cs variable for correctness
-	cs = atoi(argv[2]);
-	
-	if(DEBUG){ printf("cs = %d\n", cs);}
-
-	while(cs < 0 ){
-		printf("Error: You specified a context switch time of less than zero.\ncs = ");
-		fscanf(stdin, "%d", &cs);
-		if(DEBUG){ printf("cs = %d\n", cs);}
-	}
-
-
-	//checks the type for correctness
-	type = argv[3];
-
-	if(DEBUG){ printf("Specified type: %s\n", type);}
-
-	while(strcmp(type,"FCFS") && strcmp(type,"RR") && strcmp(type,"SJF")){
-		printf("Error: The type you specified is not FCFS, RR, or SJF. Please respecify the type.\nType = ");
-		fscanf(stdin, "%s", type);
-		if(DEBUG){ printf("type = %s\n", type);}
-	}
-
-
-	if(!strcmp(type,"RR")){
-		if(argc < 4){
-			printf("Please specify a time quantum for RR.\nq = ");
-			fscanf(stdin,"%d",&q);
-			while(q < 0){
-				
-			}
-		}	
-	}
+/* 
+ * Print program usage line to stdout. Takes string pn, which is the program 
+ * name (typically argv[0]).
+ */
+void print_usage(const char *pn) {
+	printf("Usage: %s <nprocs> <switch time> <RR|FCFS|SJF> [RR quantum]\n", pn);
 }
 
-int main(int argc, char *argv[]){
 
-	checkArguments(argc, argv);
+/* Program entry point. */
+int main(int argc, char *argv[]){
+	if (argc < 4) {
+		print_usage(argv[0]);
+		return 1;
+	}
+
+	/* Get common command line arguments. */
+	int nproc = atoi(argv[1]);
+	int cs = atoi(argv[2]);
+	const char *type = argv[3];
+
+	/* Verify common arguments. */
+	if (nproc < 1) {
+		fprintf(stderr, "Error: nprocs must be greater than zero.\n");
+		return 1;
+	}
+
+	if (cs < 0) {
+		fprintf(stderr, "Error: switch time must be non-negative.\n");
+		return 1;
+	}
+
+	/* Branch based on type. */
+	if (strcmp(type, "FCFS") == 0) {
+		/* TODO: Run schedule function. */
+	} else if (strcmp(type, "RR") == 0) {
+		/* Need time quantum for RR. */
+		if (argc < 5) {
+			fprintf(stderr, "Error: must specify quantum for RR.\n");
+			return 1;
+		}
+
+		/* Verify time quantum is positive. */
+		int q = atoi(argv[4]);
+
+		if (q < 0) {
+			fprintf(stderr, "Error: time quantum must be positive.\n");
+		}
+
+		/* TODO: Run schedule function. */
+	} else if (strcmp(type, "SJF") {
+		/* TODO: Run schedule function. */
+	} else {
+		fprintf(stderr, "Error: Unknown type `%s'\n", type);
+		return 1;
+	}
 
 	return 0;
 }
